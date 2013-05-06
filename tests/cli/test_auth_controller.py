e22544079acf7c6585e06a1156655bd722762f25
Bernardo Heynemann <heynemann@gmail.com>
2013-05-06 11:57:31 -0300

    Authentication Controller

--- tests/cli/test_auth_controller.py
+++ tests/cli/test_auth_controller.py
@@ -46,3 +46,19 @@ class AuthControllerTestCase(FullTestCase):
 
         expect(mock_stdout.getvalue()).to_be_like("Aborting...")
         expect(ask_for_mock.called).to_be_true()
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    @mock.patch.object(AuthController, 'get_pass')
+    @mock.patch.object(AuthController, 'ask_for')
+    def test_default_action_when_email_is_none(self, ask_for_mock, get_pass_mock, mock_stdout):
+        ask_for_mock.return_value = "test@test.com"
+        get_pass_mock.return_value = None
+
+        ctrl = self.make_controller(AuthController, conf=self.fixture_for('test.conf'), email=None, password=None)
+        ctrl.app.user_data = UserData(target="test-target")
+
+        expect(ctrl.default()).to_be_false()
+
+        expect(mock_stdout.getvalue()).to_be_like("Aborting...")
+        expect(ask_for_mock.called).to_be_true()
+        expect(get_pass_mock.called).to_be_true()
