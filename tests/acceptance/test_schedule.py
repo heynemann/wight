2de5d15d0e0350024850c53dac7b60614a747d5c
Bernardo Heynemann <heynemann@gmail.com>
2013-05-15 15:54:38 -0300

    Acceptance tests for scheduling

---
+++ tests/acceptance/test_schedule.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# wight load testing
+# https://github.com/heynemann/wight
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2013 Bernardo Heynemann heynemann@gmail.com
+
+from preggy import expect
+
+from wight.models import LoadTest
+from tests.acceptance.base import AcceptanceTest
+from tests.factories import TeamFactory
+
+
+class TestSchedule(AcceptanceTest):
+
+    def test_can_schedule(self):
+        team = TeamFactory.create(owner=self.user)
+        TeamFactory.add_projects(team, 2)
+        project_name = team.projects[0].name
+
+        result = self.execute("schedule", team.name, project_name)
+        expect(result).to_be_like(
+            "Scheduled a new load test for project '%s' in team '%s' at '%s' target." % (
+                project_name, team.name, self.target
+            )
+        )
+
+        load_test = LoadTest.objects.filter(team=team, project_name=project_name).first()
+        expect(load_test).not_to_be_null()
+
+        expect(load_test.team.id).to_equal(team.id)
+        expect(load_test.project_name).to_equal(project_name)
+        expect(load_test.created_by.id).to_equal(self.user.id)
