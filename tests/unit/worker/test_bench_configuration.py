ebd7ca1d54cdd18dbf593b2de5e56274c98b45c5
Bernardo Heynemann <heynemann@gmail.com>
2013-08-13 14:43:02 -0300

    Fixed test

--- tests/unit/worker/test_bench_configuration.py
+++ tests/unit/worker/test_bench_configuration.py
@@ -13,7 +13,6 @@ from preggy import expect
 from tests.unit.base import WorkerTestCase
 
 import mock
-import sys
 
 from wight.worker.bench_configuration import BenchConfiguration
 
@@ -22,13 +21,13 @@ class TestBenchConfiguration(WorkerTestCase):
 
     @mock.patch('wight.worker.bench_configuration.sys', executable='/bin/python')
     def test_generate_conf_with_workers_password(self, executable_mock):
-        bench = BenchConfiguration('test', 'test', 'test', workers = [
-                    {
-                        "host": "ma-amazing-host",
-                        "user": "user",
-                        "password": "password",
-                    },
-                ])
+        bench = BenchConfiguration('test', 'test', 'test', workers=[
+            {
+                "host": "ma-amazing-host",
+                "user": "user",
+                "password": "password",
+            },
+        ])
         with NamedTemporaryFile() as temp_file:
             bench.save(temp_file.name)
             expect(temp_file.read()).to_be_like('''
@@ -47,6 +46,7 @@ sleep_time_max = 0.50
 [test]
 description=test
 [distribute]
+channel_timeout = 300
 log_path =
 python_bin = /bin/python
 funkload_location=https://github.com/nuxeo/FunkLoad/archive/master.zip
@@ -87,6 +87,7 @@ sleep_time_max = 0.50
 [test]
 description=test
 [distribute]
+channel_timeout = 300
 log_path =
 python_bin = /bin/python
 funkload_location=https://github.com/nuxeo/FunkLoad/archive/master.zip
