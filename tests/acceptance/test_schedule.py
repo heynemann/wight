41d11d99f89936dbb0a33d6a0bbe76ccdd9d37b5
marcio.santana <marcio.santana@corp.globo.com>
2013-08-15 18:26:15 -0300

    acceptance test to schedule with team and project default

--- tests/acceptance/test_schedule.py
+++ tests/acceptance/test_schedule.py
@@ -36,3 +36,63 @@ class TestSchedule(AcceptanceTest):
         expect(load_test.pressure).to_equal("medium")
         expect(load_test.project_name).to_equal(project_name)
         expect(load_test.created_by.id).to_equal(self.user.id)
+
+    def test_can_schedule_with_default_team(self):
+        team = TeamFactory.create(owner=self.user)
+        TeamFactory.add_projects(team, 2)
+        project_name = team.projects[0].name
+        self.execute("default-set", team=team.name)
+        result = self.execute("schedule", project=project_name, url="http://www.globo.com")
+        expect(result).to_be_like(
+            "Scheduled a new load test for project '%s' in team '%s' at '%s' target." % (
+                project_name, team.name, self.target
+            )
+        )
+
+        load_test = LoadTest.objects.filter(team=team, project_name=project_name).first()
+        expect(load_test).not_to_be_null()
+
+        expect(load_test.team.id).to_equal(team.id)
+        expect(load_test.pressure).to_equal("medium")
+        expect(load_test.project_name).to_equal(project_name)
+        expect(load_test.created_by.id).to_equal(self.user.id)
+
+    def test_can_schedule_with_default_project(self):
+        team = TeamFactory.create(owner=self.user)
+        TeamFactory.add_projects(team, 2)
+        project_name = team.projects[0].name
+        self.execute("default-set", project=project_name)
+        result = self.execute("schedule", team=team.name, url="http://www.globo.com")
+        expect(result).to_be_like(
+            "Scheduled a new load test for project '%s' in team '%s' at '%s' target." % (
+                project_name, team.name, self.target
+            )
+        )
+
+        load_test = LoadTest.objects.filter(team=team, project_name=project_name).first()
+        expect(load_test).not_to_be_null()
+
+        expect(load_test.team.id).to_equal(team.id)
+        expect(load_test.pressure).to_equal("medium")
+        expect(load_test.project_name).to_equal(project_name)
+        expect(load_test.created_by.id).to_equal(self.user.id)
+
+    def test_can_schedule_with_default_team_and_project(self):
+        team = TeamFactory.create(owner=self.user)
+        TeamFactory.add_projects(team, 2)
+        project_name = team.projects[0].name
+        self.execute("default-set", project=project_name, team=team.name)
+        result = self.execute("schedule", url="http://www.globo.com")
+        expect(result).to_be_like(
+            "Scheduled a new load test for project '%s' in team '%s' at '%s' target." % (
+                project_name, team.name, self.target
+            )
+        )
+
+        load_test = LoadTest.objects.filter(team=team, project_name=project_name).first()
+        expect(load_test).not_to_be_null()
+
+        expect(load_test.team.id).to_equal(team.id)
+        expect(load_test.pressure).to_equal("medium")
+        expect(load_test.project_name).to_equal(project_name)
+        expect(load_test.created_by.id).to_equal(self.user.id)
