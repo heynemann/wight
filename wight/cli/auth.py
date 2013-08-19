48bf3a99e1b17a4a099a610697010589fa097b95
Bernardo Heynemann <heynemann@gmail.com>
2013-08-19 16:52:02 -0300

    Tests working

--- wight/cli/auth.py
+++ wight/cli/auth.py
@@ -8,7 +8,10 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2013 Bernardo Heynemann heynemann@gmail.com
 
+import sys
+
 from cement.core import controller
+import cement.core.exc
 
 from wight.cli.base import WightBaseController, connected_controller
 
@@ -30,62 +33,71 @@ class AuthController(WightBaseController):
         with connected_controller(self):
             self.log.info("Authenticating with %s." % self.app.user_data.target)
 
-            email = self.arguments.email
-            if email is None:
-                self.line_break()
-                email = self.ask_for("%sPlease enter the %se-mail%s to authenticate with:" % (
-                    self.reset, self.keyword_color, self.reset)
-                )
+            try:
+                email = self.arguments.email
+                if email is None:
+                    self.line_break()
+                    email = self.ask_for("%sPlease enter the %se-mail%s to authenticate with:" % (
+                        self.reset, self.keyword_color, self.reset)
+                    )
 
-            if not email:
-                self.abort()
-                return False
-
-            password = self.arguments.password
-            if password is None:
-                password = self.get_pass("%sPlease enter the %spassword%s to authenticate with (nothing will be displayed):" % (
-                    self.reset, self.keyword_color, self.reset)
-                )
-
-            if not password:
-                self.abort()
-                return False
-
-            response = self.get("/auth/user", headers={
-                'email': email,
-                'password': password
-            })
-
-            if response.status_code == 400:
-                self.abort("Invalid email or password")
-                return False
-            if response.status_code == 403:
-                self.abort("Authentication failed.")
-                return False
-            elif response.status_code == 404:
-                register = self.ask_for("%sUser does not exist. Do you wish to register? [%sy/n%s]" % (
-                    self.reset, self.keyword_color, self.reset)
-                )
-
-                if not register or register.lower() not in ("y", "n") or register.lower() == "n":
+                if not email:
                     self.abort()
                     return False
 
-                response = self.get("/auth/register", headers={
+                password = self.arguments.password
+                if password is None:
+                    password = self.get_pass("%sPlease enter the %spassword%s to authenticate with (nothing will be displayed):" % (
+                        self.reset, self.keyword_color, self.reset)
+                    )
+
+                if not password:
+                    self.abort()
+                    return False
+
+                response = self.get("/auth/user", headers={
                     'email': email,
                     'password': password
                 })
 
-                self.line_break()
-                self.putsuccess("User registered and authenticated.")
-                self.line_break()
-            elif response.status_code == 200:
-                self.line_break()
-                self.putsuccess("Authenticated.")
-                self.line_break()
+                if response.status_code == 400:
+                    self.abort("Invalid email or password")
+                    return False
+                if response.status_code == 403:
+                    self.abort("Authentication failed.")
+                    return False
+                elif response.status_code == 404:
+                    register = self.ask_for("%sUser does not exist. Do you wish to register? [%sy/n%s]" % (
+                        self.reset, self.keyword_color, self.reset)
+                    )
 
-            self.__update_token(response)
-        return True
+                    if not register or register.lower() not in ("y", "n") or register.lower() == "n":
+                        self.abort()
+                        return False
+
+                    response = self.get("/auth/register", headers={
+                        'email': email,
+                        'password': password
+                    })
+
+                    self.line_break()
+                    self.putsuccess("User registered and authenticated.")
+                    self.line_break()
+                elif response.status_code == 200:
+                    self.line_break()
+                    self.putsuccess("Authenticated.")
+                    self.line_break()
+
+                self.__update_token(response)
+                return True
+            except cement.core.exc.CaughtSignal:
+                exc = sys.exc_info()[1]
+
+                if exc.signum == 2:
+                    self.abort("Authentication aborted.")
+                    return False
+                else:
+                    raise
 
     def __update_token(self, response):
         token = response.headers.get('Token')
