1ec39828e95973499555fe4beff157da5a553016
marcio.santana <marcio.santana@corp.globo.com>
2013-08-21 15:36:30 -0300

    schedule cli had simple paramenter.

--- tests/acceptance/test_schedule.py
+++ tests/acceptance/test_schedule.py
@@ -36,6 +36,25 @@ class TestSchedule(AcceptanceTest):
         expect(load_test.project_name).to_equal(project_name)
         expect(load_test.created_by.id).to_equal(self.user.id)
 
+    def test_can_schedule_simple(self):
+        team = TeamFactory.create(owner=self.user)
+        TeamFactory.add_projects(team, 2)
+        project_name = team.projects[0].name
+
+        result = self.execute("schedule", team=team.name, project=project_name, url="http://www.globo.com", simple=True)
+        expect(result).to_be_like(
+            "Scheduled a new load test for project '%s' in team '%s' at '%s' target." % (
+                project_name, team.name, self.target
+            )
+        )
+
+        load_test = LoadTest.objects.filter(team=team, project_name=project_name).first()
+        expect(load_test).not_to_be_null()
+
+        expect(load_test.team.id).to_equal(team.id)
+        expect(load_test.project_name).to_equal(project_name)
+        expect(load_test.created_by.id).to_equal(self.user.id)
+
     def test_can_schedule_with_default_team(self):
         team = TeamFactory.create(owner=self.user)
         TeamFactory.add_projects(team, 2)
