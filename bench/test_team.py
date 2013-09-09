66fe5a61fcb00fc5f3bb1fda751cdfa62ea66d46
marcio.santana <marcio.santana@corp.globo.com>
2013-09-09 11:56:00 -0300

    team show funkload test

---
+++ bench/test_team.py
@@ -0,0 +1,14 @@
+import unittest
+
+from funkload.FunkLoadTestCase import FunkLoadTestCase
+
+
+class ShowTeamTest(FunkLoadTestCase):
+    def setUp(self):
+        self.server_url = '%s/teams/' % (self.conf_get('main', 'url').rstrip('/'),)
+
+    def test_show_team(self):
+        self.get("%/teste" % self.server_url, description='show team url')
+
+if __name__ == '__main__':
+    unittest.main()
