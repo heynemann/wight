070b549db12bc73108f8f01288dabf73f5f608a9
marcio.santana <marcio.santana@corp.globo.com>
2013-09-09 12:03:09 -0300

    fixing test.

--- bench/test_team.py
+++ bench/test_team.py
@@ -5,7 +5,7 @@ from funkload.FunkLoadTestCase import FunkLoadTestCase
 
 class ShowTeamTest(FunkLoadTestCase):
     def setUp(self):
-        self.server_url = '%s/teams/' % (self.conf_get('main', 'url').rstrip('/'),)
+        self.server_url = '%s/teams' % (self.conf_get('main', 'url').rstrip('/'),)
 
     def test_show_team(self):
         self.get("%s/teste" % self.server_url, description='show team url')
